3896ff313b3d35620bd01724be3bfb96b6beb966
Blonded <Lena@lenas-mbp.attlocal.net>
2018-04-26 23:08:27 -0700

    update game

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -1,3 +1,141 @@
-// alert("Hello");
+// dictionary of all global variable that are being tracked in the game
+// initial game values
+var gameData = {
+    wins: 0,
+    losses: 0,
+    guessesLeft: 10,
+    guessedLetters: [],
+    computerGuess: generator()
+}
+
+// initial values displayed on the page
+document.querySelector("#wins").innerHTML = gameData.wins;
+document.querySelector("#losses").innerHTML = gameData.losses;
+document.querySelector("#guesses-left").innerHTML = gameData.guessesLeft;
+
+// generate a random letter a-z
+function generator(){
+    // may need to lowercase the computer guess
+    // use ascii value of letter
+    return String.fromCharCode(
+        Math.floor(Math.random() * 26) + 97
+      )
+}
+
+// for testing, computer initial guess
+console.log(gameData.computerGuess);
+
+
+// game logic
+// ask user for a letter;
+document.onkeyup = function(event) {
+
+    document.querySelector("#alert").innerHTML = "";
+    document.querySelector("#play-again").innerHTML = "";
+     // if the user still has guesses remaining
+ 
+     // check how many guesses are left
+    if(gameData.guessesLeft > 0){
+        // store user input in a variable. 
+        // lowercase user input
+        var userInput = event.key.toLowerCase();
+
+        // use ascii code of key press value to do check 
+        // make sure its not a number or a character other than a-z
+        if(event.keyCode >= 65 && event.keyCode <= 90){
+            // console.log("user selected a letter:", userInput);
+
+            // check if user has already guessed the letter by checking if letter guessed is in a array of guesses
+            if(gameData.guessedLetters.includes(userInput)){
+                console.log("User has already guessed this letter!");
+                document.querySelector("#alert").innerHTML = "User has already guessed this letter!";
+            }
+             // if the letter is not in the array add it else tell user to try another letter
+            else {
+            // if the letter is not in array also minus one from the guesses left
+                
+                gameData.guessedLetters.push(userInput);
+
+                document.querySelector("#guessed-letters").innerHTML = gameData.guessedLetters.join(", ");
+
+
+                gameData.guessesLeft--;
+                document.querySelector("#guesses-left").innerHTML = gameData.guessesLeft;
+                
+                // console.log("guesses left", gameData.guessesLeft)
+                // console.log("user guess added to array");
+
+                // compare the letter to the computer guess 
+                compareGuess(userInput);
+            }
+            }
+    }
+    else{
+        console.log("You Lost");
+        handleLosses();
+    }
+}
+
+// compare user input (letter) to computer letter
+function compareGuess(letter){
+    // turnary operator
+    (gameData.computerGuess === letter)? 
+        // console.log("user got it right")
+        handleWin()
+
+   : 
+        //console.log("user got it wrong")
+        // if incorrect and user still has guesses run all steps above again
+        document.querySelector("#alert").innerHTML = "That was not correct! Try again!"
+}
+
+function handleWin(){
+    // console.log("$$$$gameData.wins", gameData.wins)
+    // updateScore(gameData.wins)
+
+    // if letter guessed is correct wins go up
+    gameData.wins++
+    // console.log("###gameData.wins", gameData.wins)
+    document.querySelector("#wins").innerHTML = gameData.wins;
+    document.querySelector("#alert").innerHTML = "YOU WIN!";
+    // reset game
+    resetGame()
+}
+
+function handleLosses(){
+    // updateScore(gameData.losses);
+    // reset game
+     // if letter guessed is incorrect and user out of guesses, losses go up
+    gameData.losses++
+
+    document.querySelector("#losses").innerHTML = gameData.losses;
+    document.querySelector("#alert").innerHTML = "YOU LOSE!";
+    // console.log("###gameData.losses", gameData.losses)
+
+    resetGame();
+}
+
+// reset game after a win or a losse
+function resetGame(){
+    document.querySelector("#play-again").innerHTML = "Would you like to play again? Guess another letter!";
+    gameData.guessesLeft = 10
+    document.querySelector("#guesses-left").innerHTML = gameData.guessesLeft;
+
+    gameData.guessedLetters = []
+    document.querySelector("#guessed-letters").innerHTML = ""
+    
+    gameData.computerGuess =  generator()
+
+    // for testing computer new guess
+    console.log(gameData.computerGuess)
+
+}
+
+ 
+
+ 
+
+
+
 
 
--- README.md
+++ README.md
@@ -1,22 +1,44 @@
 # Psychic-Game
-Psychic Game, Guessing numbers while implementing Javascript with CSS, HTML, and Bootstrap.
+Psychic Game, 
+Guessing numbers while implementing Javascript with CSS, and HTML.
+
+This is a game where you have to guess what letter the computer is thinking of. Every time you type 'one' letter it will print out on the page whether you are wrong or right. It will also give you the amount of losses & wins with the amount of tries you have left. 
+
+This game will automatically reset when you lose or win by typing in another letter.
+
+Psuedocode notes put throughout to understand functionality of specific areas of code.
 
 Wk 2 Assignment 4 - UCB
 
 <a href="https://blonded.github.io/Psychic-Game/" alt="DEMO"> "DEMO" </a>
 
 Installing
-Download repository on GitHub, open in preferred text editor to view source code (i.e. Atom, Sublime, Visual Studio Code). Open index.html, style.css, and psychicgame.js. Right click on the html file code and select "view in default browser" to view a demo of the website created. (Google Chrome Preferred for viewing)
+Download repository on GitHub, open in preferred text editor to view source code (i.e. Atom, Sublime, Visual Studio Code). Open index.html, style.css, and game.js. Right click on the html file code and select "view in default browser" to view a demo of the website created. (Google Chrome Preferred for viewing)
 
 index.html
 style.css
 game.js
 README.md
 
-<img src="" alt="">
+
+<img src="PsychicGame.jpg" alt="Demo-photo">
+
+Sample of JavaScript used:
+
+This function creates a random number and converts it using the Ascii number chart to then create a letter, A-Z (lowercase only).
 
 '''''
-SAMPLE CODE HERE
+function generator(){
+    // may need to lowercase the computer guess
+    // use ascii value of letter
+    return String.fromCharCode(
+        Math.floor(Math.random() * 26) + 97
+      )
+}
+
+// for testing, computer initial guess
+console.log(gameData.computerGuess);
+
 
 '''''
 
@@ -25,10 +47,8 @@ SAMPLE CODE HERE
 * [html]
 * [css]
 * [javascript]
-* [bootstrap]
 
 ## Authors
 
 * **Lena Martinson** - *Github* - [Github](https://github.com/Blonded)
-* **BootStrap** - *BootStrap* - [BootStrap](https://getbootstrap.com/)
 * **Google Fonts** - *GoogleFonts* - [Google Fonts](https://fonts.google.com/)
